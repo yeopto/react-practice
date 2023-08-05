07ce4edd5c6b6a8468b20abd56e042efa55cd724
yeopto <gunyeop530@gmail.com>
2023-08-05 23:29:41 +0900

    chore: suspense-errorboundary initial commit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## 개요
 
-[React 완벽 가이드](https://www.udemy.com/course/best-react/) 강의를 보며 필요한 것을 학습하는 레포지토리
+[React 완벽 가이드](https://www.udemy.com/course/best-react/) 강의를 보며 필요한 것을 학습하고 리액트 연습하는 레포지토리
---
+++ suspense-errorboundary/src/mocks/handlers.ts
@@ -0,0 +1,35 @@
+import { rest } from 'msw';
+
+const name = 'yeopto';
+
+const todos = [
+  {
+    id: 1,
+    todo: '먹고',
+  },
+  {
+    id: 2,
+    todo: '자고',
+  },
+  {
+    id: 3,
+    todo: '놀기',
+  },
+];
+
+export const handlers = [
+  // 할일 목록
+  rest.get('/todos', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(todos), ctx.delay(1000));
+  }),
+
+  rest.get('/name', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(name), ctx.delay(1000));
+  }),
+
+  // 할일 추가
+  rest.post('/todos', async (req, res, ctx) => {
+    todos.push(await req.json());
+    return res(ctx.status(201));
+  }),
+];
